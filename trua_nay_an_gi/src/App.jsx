5ec3a456f3fdb9eed80994a124c6aaf13e184e9d
Quốc Nguyễn <nguyenminhquoc812005@gmail.com>
2025-04-18 21:07:22 +0700

    react jsx

---
+++ trua_nay_an_gi/src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import NavbarWeb from './components/Navigate/NavbarWeb';
+import Header from './pages/Home/Header';
+import About from './pages/About/About';
+import Team from './pages/Team/Team';
+import Footer from './components/Footer/Footer';
+import Profile from './pages/Profile/Profile';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('Error:', error);
+    console.log('Error Info:', errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <div className="App">
+        <NavbarWeb />
+        <Header />
+        <About />
+        <Profile />
+        <Team />
+        <Footer />
+      </div>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
